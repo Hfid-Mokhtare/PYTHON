06caff666e879621eb141807db63fa0e910c2101
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2024-11-25 14:42:22 +0100

    Create Lesson #01 - Output (Print).py

---
+++ Lesson #01 - Output (Print).py
@@ -0,0 +1,17 @@
+print("I Love Programmming!\n")
+print("I promise to be the best developer ever!\n")
+print("I know it will take some time to practice ,but I will achieve my goal.\n")
+print("Best Rrgards,\n")
+print("Mohammed Ali Zeggaf.\n")
+
+########################################################
+
+print("\n*********************************************************\n")
+print("Name : Mohammed Ali Zeggaf.\n")
+print("Age : 18 Years.\n")
+print("City : tangier\n")
+print("Country : Morocco\n")
+print("*********************************************************\n")
+
+
+
