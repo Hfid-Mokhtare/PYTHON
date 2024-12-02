1a349635d51a14bb3be8d63d709c98b0ca145969
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2024-12-02 16:25:07 +0100

    Create Lesson #09 : Math Functions in Python.py

---
+++ Lesson #09 : Math Functions in Python.py
@@ -0,0 +1,23 @@
+import math
+
+Num=float(input("please enter a number :"))
+
+# Square root
+sqrt_result = math.sqrt(Num)
+print("Square root:", sqrt_result)
+
+# Power
+power_result = pow(Num, 2)
+print("Power of 2:", power_result)
+
+# Ceiling
+ceil_result = math.ceil(Num)
+print("Ceiling:", ceil_result)
+
+# Floor
+floor_result = math.floor(Num)
+print("Floor:", floor_result)
+
+# Round to 1 decimal place
+round_result = round(Num, 1)
+print("Rounded:", round_result)
