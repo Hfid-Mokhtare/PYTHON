582185d3b32654a8fb869ee20617952f59220873
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2024-12-02 14:44:23 +0100

    Create Lesson #05 : User Inputs.py

---
+++ Lesson #05 : User Inputs.py
@@ -0,0 +1,50 @@
+#Q1 : 
+
+FullName = str(input("Full Name : "))
+Age = int(input("Age : "))
+City = str(input("City : "))
+Country = str(input("Country : "))
+MonthlySalary = int(input("Month Salary : "))
+YearlySalary = MonthlySalary * 12
+Gender = str(input("Gender : "))
+isMarried = bool(input("Is Married : "))
+
+print("************************************")
+print("Name : ",  FullName ,"\n")
+print("Age : " , Age ," Years. \n")
+print("City : ",City," \n")
+print("Monthly Salary : : " , MonthlySalary , " \n")
+print("Yearly Salary : " , YearlySalary , " \n")
+print("Gender :" , Gender , " \n")
+print("Married  : " , bool(isMarried)  , " \n")
+print("************************************")
+
+#------------------------------------------------------------------
+
+#Q2 : 
+
+Num1=int(input("Number 1 : "))
+Num2=int(input("Number 2 : "))
+Num3=int(input("Number 3 : "))
+Total=int(Num1+Num2+Num3)
+
+print(Num1,"+")
+print(Num2,"+")
+print(Num3,"")
+print("----------------")
+print("Tota =", Total,"\n")
+
+
+#------------------------------------------------------------------
+
+#Q3 : 
+
+Age=int(input("Enter Age : "))
+add_to_age=5
+
+print("after",add_to_age,"years you will be",Age+add_to_age,"years old\n")
+
+
+
+
+
