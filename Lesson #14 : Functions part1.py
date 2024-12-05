69d1ff1bbe89d7029ce6919caac1f4a28f73f9e6
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2024-12-05 13:20:37 +0100

    Create Lesson #14 : Functions part1.py

---
+++ Lesson #14 : Functions part1.py
@@ -0,0 +1,13 @@
+def Print_My_Card_Info():
+    print("*********************************************************")
+    print("Name : Mohammed Ali Zeggaf.")
+    print("Age : 18 Years.")
+    print("City : tangier")
+    print("Country : Morocco")
+    print("*********************************************************")
+
+# def Print_Squire():
+#     for _ in range(4):
+#         print("*" * 10)
+
+Print_My_Card_Info()
