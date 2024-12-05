8aee1e1e89cf6aea89b3a57813beda0eeb9bb5e5
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2024-12-05 13:09:34 +0100

    Create Lesson #13 : Strings.py

---
+++ Lesson #13 : Strings.py
@@ -0,0 +1,14 @@
+st1:str
+st2:str
+st2:str
+
+st1=input("Please enter string 1 : ")
+st2=input("Please enter string 2 : ")
+st3=input("Please enter string 3 : ")
+
+print("___________________________\n")
+print("the length of string1 is :",len(st1))
+print("caracters at 2 , 4 , 5 , 7 or string 1 are :",st1[1],st1[3],st1[4],st1[6])
+print("Concatunating string 1 and string 2 : ",st1+st2)
+
+
