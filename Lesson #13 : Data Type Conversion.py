bd7fa7f9ecafb28c7758e9dd6f2ce1a455c01d46
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2024-12-05 12:45:05 +0100

    Create Lesson #13 : Data Type Conversion.py

---
+++ Lesson #13 : Data Type Conversion.py
@@ -0,0 +1,20 @@
+st1: str="43.22"
+N1:int=20
+N2:float=33.5
+
+print("st1 = ",st1)
+print("N1 = ",N1)
+print("N2 = ",N2)
+
+
+print( "string \"st1\" to float : " ,float(st1) )
+
+print( "integer \"N1\" to string : " ,str(N1) )
+print( "float \"N2\" to string : " ,str(N2) )
+
+print( "integer \"N1\" to float : " ,float(N1) )
+
+
+print( "float \"N2\" to string : " ,str(N2) )
+print( "float \"N2\" to int : " ,int(N2) )
+
