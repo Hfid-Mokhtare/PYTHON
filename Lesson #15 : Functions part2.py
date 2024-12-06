516f545d1af5091cefdcb535c6d2ff1b79e91ec9
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2024-12-06 08:52:34 +0100

    Create Lesson #15 : Functions part2.py

---
+++ Lesson #15 : Functions part2.py
@@ -0,0 +1,20 @@
+def MySumProcedure():
+    Num1:int 
+    Num2:int
+    Num1=int(input("Please enter Number 1 : "))
+    Num2=int(input("Please enter Number 2 : "))
+    print("-----------------------------")
+    print(Num1+Num2)
+
+def MySumfumction():
+    Num1:int 
+    Num2:int
+    Num1=int(input("Please enter Number 1 : "))
+    Num2=int(input("Please enter Number 2 : "))
+    print("-----------------------------")
+    return Num1+Num2
+
+
+MySumProcedure()
+print("\n")
+print(MySumfumction())
