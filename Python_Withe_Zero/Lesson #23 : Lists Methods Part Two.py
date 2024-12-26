b83ccd2ce59992db473847ff4d20c775996cd550
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2024-12-26 14:15:56 +0100

    Create Lesson #23 : Lists Methods Part Two.py

---
+++ Python_Withe_Zero/Lesson #23 : Lists Methods Part Two.py
@@ -0,0 +1,25 @@
+# clear
+
+a=[1,2,3,4]
+print (a)
+a.clear()
+print(a)
+
+# copy
+
+b=[1,2,3,4]
+c=b.copy()
+b.append(5)
+print(b)
+print(c)
+
+# index
+
+e=["Ali","Ahmad","Mohamed"]
+print(e.index("Mohamed"))
+
+# insert
+
+f=[1,2,3,4,5,"A","B"]
+f.insert(0,"c_test")
+print(f)
