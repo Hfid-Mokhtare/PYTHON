2b2b4e15a6b450a5df3f3b779026f203e09bcb22
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2024-12-28 15:07:26 +0100

    Create Lesson #34 : Tuples And Methods Part One.py

---
+++ Python_Withe_Zero/Lesson #34 : Tuples And Methods Part One.py
@@ -0,0 +1,42 @@
+# ------------------------------
+# -------- Tuple -------
+# ------------------------------
+
+# [1] Tuple items are enclosed in parentheses ()
+# [2] you can remove the parenthese if you want
+# [3] Tuple Are ordered, to use index to access item
+# [4] Tuple are immutable => you cant add or delete (no change)
+# [5] Tuple items is not unique
+# [6] Tuple can have Different Data types
+# [7] Operators Used in Strings and Lists Available in Tuples
+# --------------------------------------------------------------
+
+
+MyTuple1=("Ali", "Mohamed")
+MyTuple2= "Ali", "Mohamed"
+
+print(type(MyTuple1))
+print(type(MyTuple2))
+
+# Tuple indexing
+
+MyTuple3=(1,2,3,4,5,6,7)
+print(MyTuple3[0])
+print(MyTuple3[1])
+print(MyTuple3[-2])
+
+# Tuple Assign Values
+
+MyTuple4=(1,2,3,"ali","mohamed")
+# MyTuple4[3]="Reda"
+# print(MyTuple4) # 'tuple' object does not support item assignment
+
+
+# Tuple Items
+MyTuple5=("omar","ali","ossama",True,10.5,3)
+print(MyTuple5)
+
+
+
+
+ 
