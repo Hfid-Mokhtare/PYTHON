ce82c36eee2f0852ec45cd30b15e20289141e321
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-01-06 18:09:22 +0100

    Create For Loops.py

---
+++ PythonLevel1/For Loops.py
@@ -0,0 +1,43 @@
+def ReadNum():
+    Num=int(input("enter the number : "))
+    return Num
+def PrintResult(Num):
+    for i in range(1,Num+1):
+        print(i)
+PrintResult(ReadNum())
+
+#---------------------------------------------
+
+def ReadNum():
+    Num=int(input("enter the number : "))
+    return Num
+def PrintResult(Num):
+    for i in range(Num,0,-1):
+        print(i)
+PrintResult(ReadNum())
+
+#---------------------------------------------
+
+def ReadNum():
+    Num=int(input("enter the number : "))
+    return Num
+def PrintSumOddNumbers(Num):
+    SumOddNum=0
+    for i in range(1,Num+1,2):
+        SumOddNum+=i
+    print(SumOddNum)
+
+PrintSumOddNumbers(ReadNum())
+
+#---------------------------------------------
+
+def ReadNum():
+    Num=int(input("enter the number : "))
+    return Num
+def PrintSumEvenNumbers(Num):
+    SumEvenNum=0
+    for i in range(0,Num+1,2):
+        SumEvenNum+=i
+    print(SumEvenNum)
+
+PrintSumEvenNumbers(ReadNum())
