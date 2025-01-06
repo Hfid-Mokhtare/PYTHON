1f05ef65df4cd936975a4f61a3c44b4a8e41b6f3
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-01-06 14:35:40 +0100

    Update Lesson #11 : Dictionaries.py

--- PythonLevel1/Lesson #11 : Dictionaries.py
+++ PythonLevel1/Lesson #11 : Dictionaries.py
@@ -1,3 +1,37 @@
+# using dictionary and for loop ------------------------------------------------------------------------
+
+ContacteInfo={
+    "facebook_acounte":str,
+    "tweeter_acounte":str,
+    "emaile":str,
+    "phone":str
+}
+
+PersonalInfo={
+    "FullName":str,
+    "Age":int,
+    "gender":chr
+
+}
+
+CartInfo={
+    "ContacteInfo":ContacteInfo,
+    "PersonalInfo":PersonalInfo
+}
+
+for data_k, data_v in CartInfo.items():
+    for info_k in data_v:
+
+        data_v[info_k]=input(f"enter {info_k} : ")
+
+print("-"*50)
+
+for data_k, data_v in CartInfo.items():
+    for info_k, info_v in data_v.items():
+
+        print(f"{info_k} : {info_v}")
+
+
 person = {
     "name": "Alice",
     "age": 30,
@@ -85,5 +119,6 @@ def main():
     print("\tphone:", My_Cart_Info.ContacteInfo.phone)
 
 main()
+
 
 
