529d539c5f5286ccb03c47157ed095eb337d23b5
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-01-06 17:29:44 +0100

    Update Lesson #25 : enums with if else statement.py

--- PythonLevel1/Lesson #25 : enums with if else statement.py
+++ PythonLevel1/Lesson #25 : enums with if else statement.py
@@ -1,3 +1,15 @@
+# you can use the class : 
+# class Day(Enum):
+#     MONDAY = 1
+#     TUESDAY = 2
+#     WEDNESDAY = 3
+#     THURSDAY = 4
+#     FRIDAY = 5
+#     SATURDAY = 6
+#     SUNDAY = 7
+
+
+
 # like an enum of days
 monday = 1
 tuesday = 2  
