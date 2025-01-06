ca0336db1203cb0faa9fda72389e2b7ac0d0f391
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-01-06 17:28:06 +0100

    Create Lesson #25 : enums with if else statement.py

---
+++ PythonLevel1/Lesson #25 : enums with if else statement.py
@@ -0,0 +1,33 @@
+# like an enum of days
+monday = 1
+tuesday = 2  
+wednesday = 3 
+thursday = 4
+friday = 5
+saturday = 6
+sunday = 7
+
+def ReadNumberOfDay():
+    Day=int(input("Please enter the Number of a Day : "))
+    return Day
+
+
+def CheckGrade(NumDay):
+    if NumDay == monday : 
+       print("monday")
+    elif NumDay == tuesday:
+       print("tusday")
+    elif NumDay == wednesday:
+       print("wedensday")   
+    elif NumDay == thursday:
+       print("thursday") 
+    elif NumDay == friday:
+       print("friday")
+    elif NumDay == saturday:
+       print("saturday")
+    elif NumDay == saturday:
+       print("saturday")
+    else :
+       print("wrongue Input :( ")  
+
+CheckGrade(ReadNumberOfDay())
