641c58367d9acd4cfe7bed6584476af363a10ad3
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-01-10 18:27:20 +0100

    Update and rename POO #5 : __str__.py to POO #5 : __str__ et __del__.py

--- OOP/POO #5 : __str__ et __del__.py
+++ OOP/POO #5 : __str__ et __del__.py
@@ -3,11 +3,16 @@ class etudian:
         self.nom=n
         self.age=a
 
-     # pour remplace la methode afficher 
-     # E1.afficher ==> print(E1)   
+    # pour remplace la methode afficher 
+    # E1.afficher ==> print(E1)   
     def __str__(self):
         return (f"le nom : {self.nom}, l'age : {self.age}")
+
+     # pour afficher un message when : del E1  
+    def __del__(self):
+        print("ici la suprition de l'objet ")
 
 
 E1=etudian("zeggaf",19)
 print(E1)
+del E1
