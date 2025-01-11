361fe441d0ab3c032efedd1a53256ae6711dff6b
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-01-11 14:23:12 +0100

    Create Lesson #96 : Regular Expressions Part 2.py

---
+++ Python_Withe_Zero/Lesson #96 : Regular Expressions Part 2.py
@@ -0,0 +1,14 @@
+patter1=r"^\w*$"     # to get one or more words
+patter2=r"^\s\s\w$"  # to get two spases and a letter
+patter3=r"^\w*$"     # to get from (0 to infinity) letter 
+patter4=r"^\w+$"     # to get from (1 to infinity) letter 
+patter5=r"^\w?$"     # to get from (zero or one) letter only
+patter6=r"^\w{3}$"   # to get 3 letter only
+patter7=r"^\w{2,5}$"  # to get from (2 letter only
+
+
+
+
+
+
+
