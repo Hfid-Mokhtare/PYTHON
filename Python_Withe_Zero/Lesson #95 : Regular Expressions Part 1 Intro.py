a1525352bd3ace3d70fb2a61d220719ebd77d361
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-01-11 14:04:50 +0100

    Create Lesson #95 : Regular Expressions Part 1 Intro.py

---
+++ Python_Withe_Zero/Lesson #95 : Regular Expressions Part 1 Intro.py
@@ -0,0 +1,19 @@
+#-----------------------------------
+#-- Regular Expresion == intro -----
+#-----------------------------------
+
+# [1] it used in [Credit card validation, IP address Validation, email validation
+# [2] Test regex "https://pythex.org/"
+# [5] characters sheet "https://www.debuggex.com/cheatsheet/regex/python"
+
+#---------------------------------------------------------
+
+
+# \d : to select one degit
+# \D : to select only non degit
+
+# \s : to select only spases
+# \S : to selct only non spases
+
+# \w : to select only letters
+# \W : to selct only non letters
