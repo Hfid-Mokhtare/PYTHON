32be479a899913d932e2dbe4439659d5f6ce33d4
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-01-22 15:56:51 +0100

    Update POO #18 : le surcharge des methode.py

--- OOP/POO #18 : le surcharge des methode.py
+++ OOP/POO #18 : le surcharge des methode.py
@@ -1,3 +1,4 @@
 # le surcharge des methode faire par : 
 # le chengement dons le nombre de parametre
 # le chengement dons le type de variable on parametre => note:int || note:str
+# oubien on fait des condition if else
